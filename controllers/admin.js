bd1d56347ba718035df9ac850039ca2ef5910055
ShivanginiRathore <shivangini.atcs@gmail.com>
2023-05-18 18:51:04 +0530

    Q19-UserandProductRelation

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,8 +13,7 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  
-  Product.create({
+  req.user.createProduct({
     title: title,
     price: price,
     imageUrl: imageUrl,
@@ -33,13 +32,18 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
-  .then(prod => {
+  req.user.getProducts({where: {id: prodId}})
+  // Product.findByPk(prodId)
+  .then(products => {
+    const product = products[0];
+    if(!product){
+      return res.redirect('/');
+    }
     res.render('admin/edit-product', {
       pageTitle: 'Edit Product',
       path: '/admin/edit-product',
       editing: editMode,
-      product: prod
+      product: product
     });
   })
   .catch(err => console.log(err));
@@ -66,7 +70,8 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user.getProducts()
+  // Product.findAll()
   .then(products => {
     res.render('admin/products', {
       prods: products,
